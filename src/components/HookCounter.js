198982a6a78e89550d8981a9b7aa80c1c7686028
Mehran Mohammadi <mohammadi000098@gmail.com>
2022-07-12 13:57:09 +0430

    add count function and use callbackfunction

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -6,11 +6,23 @@ const HookCounter = () => {
   const addOneHandler = () => {
     setCount(count + 1);
   };
+  const addtwoHandler = () => {
+    setCount(count + 2);
+  };
+  const addtenHandler = () => {
+    for (let i = 0; i <= 10; i++) {
+      setCount((prevState) => {
+        return prevState + 1;
+      });
+    }
+  };
 
   return (
     <div>
       <h2>count - {count}</h2>
       <button onClick={addOneHandler}>add one</button>
+      <button onClick={addtwoHandler}>add two</button>
+      <button onClick={addtenHandler}>add ten</button>
     </div>
   );
 };
