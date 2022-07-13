18b4a5e9b1a5f7b7ffa1a5814471083db90314c2
Mehran Mohammadi <mohammadi000098@gmail.com>
2022-07-13 17:45:59 +0430

    add css

--- src/components/HookAray.js
+++ src/components/HookAray.js
@@ -1,20 +1,20 @@
 import { useState } from "react";
-
+import styles from "./product.module.css";
 const HookAray = () => {
   const [item, setitem] = useState([]);
 
   const addItemHandler = () => {
     setitem([
       ...item,
-      { id: item.length, nmber: Math.floor(Math.random) * 10 },
+      { id: item.length, number: Math.floor(Math.random() * 10) },
     ]);
   };
 
   return (
-    <div>
+    <div className={styles.product}>
       <button onClick={addItemHandler}>click</button>
       {item.map((item) => {
-        return <li key={item.id}>{item.nmber}</li>;
+        return <li key={item.id}>{item.number}</li>;
       })}
     </div>
   );
